ead0ec00125fd00ff4c952080b11d17a02ce3c35
Obinnap123 <paulobinna493@gmail.com>
2025-03-08 15:49:02 +0100

    working on responsiveness for the header

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,14 +1,20 @@
 import Logo1 from './logo.svg';
+import Hamburger from './icon-hamburger.svg';
+import Close from'./icon-close.svg';
 
 // Define an interface for the Images object
 interface Images {
     Logo1: string;
+    Hamburger:string;
+    Close:string;
     // Add more images here if needed
   }
   
 
   const Images: Images = {
     Logo1,
+    Hamburger,
+    Close,
     // Add more images here if needed
   };
 
