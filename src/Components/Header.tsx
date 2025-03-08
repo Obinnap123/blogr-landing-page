09adfecd1e70df2d142ab761c9f84d8d6dd55a6a
Obinnap123 <paulobinna493@gmail.com>
2025-03-08 10:38:41 +0100

    done with header for desktop

---
+++ src/Components/Header.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import Images from "@/images";
+// import Logo1 from '@/images/logo.svg';
+
+function Header() {
+  interface NavItem {
+    name: string;
+    link: string;
+  }
+
+  const navItems: NavItem[] = [
+    { name: "Product", link: "/Product" },
+    { name: "Company", link: "/Company" },
+    { name: "Connect", link: "/Connect" },
+  ];
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuActive((prev) => !prev);
+  };
+  return (
+    <>
+      <header className="{styles.header}">
+        <nav className="bg-[hsl(356,100%,66%)] pr-36 pl-36 pt-5 pb-10 flex gap-5 items-center justify-between">
+          <div className="flex items-center gap-10">
+            <Image src={Images.Logo1} alt="logo-image" />
+            <ul className="flex items-center gap-5 pl-5 text-gray-200 font-semibold ">
+              {navItems.map((item, index) => (
+                <li key={index} className="hover:text-black transition duration-300">
+                  <Link href={item.link}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex">
+            
+            <div className="auth flex gap-5 items-center">
+              <Link href="/login" className="text-gray-200 font-semibold hover:text-black transition duration-300">
+                Login
+              </Link>
+              <Link href="/signup" className="bg-white text-[hsl(353,100%,62%)] pt-2 pb-2 pl-8 pr-8 rounded-3xl font-semibold hover:bg-black hover:text-white transition duration-300">
+                Sign up
+              </Link>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/images/index.ts
@@ -0,0 +1,16 @@
+import Logo1 from './logo.svg';
+
+// Define an interface for the Images object
+interface Images {
+    Logo1: string;
+    // Add more images here if needed
+  }
+  
+
+  const Images: Images = {
+    Logo1,
+    // Add more images here if needed
+  };
+
+  // Export the Images object
+export default Images;
