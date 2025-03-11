0c919351c635e9cba72320e0120b03dad81fef40
Obinnap123 <paulobinna493@gmail.com>
2025-03-11 13:53:57 +0100

    still fixing the responsiveness on the header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -45,7 +45,7 @@ function Header() {
             </ul>
           </div>
           <div className="flex">
-            <div className="auth flex gap-5 items-center">
+            <div className="auth hidden md:flex gap-5 items-center">
               <Link
                 href="/login"
                 className="text-gray-200 font-semibold hover:text-black transition duration-300"
@@ -60,7 +60,52 @@ function Header() {
               </Link>
             </div>
           </div>
+          <button className="md:hidden" onClick={toggleMenu}>
+            {!menuActive && <Image src={Images.Hamburger} alt="menu-toggle" />}
+          </button>
         </nav>
+        <div
+          className={`fixed top-0 left-0 h-screen w-[100%] sm:w-[70%] md:w-[50%] shadow-lg p-5 flex flex-col items-start gap-6 transform transition-transform duration-500 ease-in-out z-50 ${
+            menuActive ? "translate-x-0" : "-translate-x-full"
+          }`}
+        >
+          {/* Close Button Inside Sidebar */}
+          <button onClick={toggleMenu} className="self-end mt-3">
+            <Image src={Images.Close} alt="close-menu" />
+          </button>
+
+          <ul className="flex flex-col gap-5 text-gray-800 font-semibold w-full pt-10">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link
+                  href={item.link}
+                  className="hover:text-[hsl(356,100%,66%)] transition duration-300 block"
+                  onClick={toggleMenu} // Close menu on click
+                >
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+
+          {/* Auth Links */}
+          <div className="flex flex-col gap-4 w-full">
+            <Link
+              href="/login"
+              className="text-gray-800 font-semibold hover:text-[hsl(356,100%,66%)] transition duration-300 block"
+              onClick={toggleMenu} // Close menu on click
+            >
+              Login
+            </Link>
+            <Link
+              href="/signup"
+              className="bg-[hsl(356,100%,66%)] text-white text-center py-2 rounded-3xl font-semibold hover:bg-black transition duration-300 block"
+              onClick={toggleMenu} // Close menu on click
+            >
+              Sign up
+            </Link>
+          </div>
+        </div>
       </header>
     </>
   );
