c39c8a8e794e43d53f4e73b6ba31242154e0078c
Obinnap123 <paulobinna493@gmail.com>
2025-03-16 23:11:13 +0100

    fixed my drop down menu for mobile screen

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -21,6 +21,7 @@ function Header() {
   const toggleMenu = () => {
     setMenuActive(!menuActive);
   };
+
   return (
     <>
       <header className="bg-[hsl(356,100%,66%)]">
@@ -33,11 +34,11 @@ function Header() {
               height={40}
               className="max-w-none md:max-w-full"
             />
-            <ul className="hidden md:flex items-center gap-5 pl-5 text-gray-200 font-semibold ">
+            <ul className="hidden md:flex items-center gap-5 pl-5 text-gray-200 font-semibold dark:text-gray-300">
               {navItems.map((item, index) => (
                 <li
                   key={index}
-                  className="hover:text-black transition duration-300"
+                  className="hover:text-black dark:hover:text-white transition duration-300"
                 >
                   <Link href={item.link}>{item.name}</Link>
                 </li>
@@ -48,13 +49,13 @@ function Header() {
             <div className="auth hidden md:flex gap-5 items-center">
               <Link
                 href="/login"
-                className="text-gray-200 font-semibold hover:text-black transition duration-300"
+                className="text-gray-200 font-semibold hover:text-black dark:text-gray-300 dark:hover:text-white transition duration-300"
               >
                 Login
               </Link>
               <Link
                 href="/signup"
-                className="bg-white text-[hsl(353,100%,62%)] pt-2 pb-2 pl-8 pr-8 rounded-3xl font-semibold hover:bg-black hover:text-white transition duration-300"
+                className="bg-white text-[hsl(353,100%,62%)] pt-2 pb-2 pl-8 pr-8 rounded-3xl font-semibold hover:bg-black hover:text-white dark:bg-gray-700 dark:text-white dark:hover:bg-gray-500 transition duration-300"
               >
                 Sign up
               </Link>
@@ -64,24 +65,33 @@ function Header() {
             {!menuActive && <Image src={Images.Hamburger} alt="menu-toggle" />}
           </button>
         </nav>
+
+        {/* Sidebar Menu */}
         <div
           className={`fixed top-0 left-0 h-screen w-[100%] sm:w-[70%] md:w-[50%] shadow-lg p-5 flex flex-col items-start gap-6 transform transition-transform duration-500 ease-in-out z-50 ${
             menuActive ? "translate-x-0" : "-translate-x-full"
-          }`}
+          } bg-gray-500 dark:bg-gray-900`}
         >
           {/* Close Button Inside Sidebar */}
-          <button onClick={toggleMenu} className="self-end mt-3">
-            <Image src={Images.Close} alt="close-menu" />
+          <button
+            onClick={toggleMenu}
+            className="self-end mt-3 p-2 bg-transparent dark:bg-transparent"
+          >
+            <Image
+              src={Images.Close}
+              alt="close-menu"
+              className=""
+            />
           </button>
 
-          <div className="h-screen bg-white w-full flex flex-col gap-6 pt-10 px-5">
+          <div className="h-screen w-full flex flex-col gap-6 pt-10 px-5">
             {/* Navigation Items */}
-            <ul className="flex flex-col gap-5 text-gray-800 font-semibold w-full">
+            <ul className="flex flex-col gap-5 text-white font-semibold dark:text-gray-300 w-full">
               {navItems.map((item, index) => (
                 <li key={index}>
                   <Link
                     href={item.link}
-                    className="hover:text-[hsl(356,100%,66%)] transition duration-300 block"
+                    className="hover:text-[hsl(356,100%,66%)] dark:hover:text-white transition duration-300 block"
                     onClick={toggleMenu}
                   >
                     {item.name}
@@ -91,17 +101,17 @@ function Header() {
             </ul>
 
             {/* Auth Links */}
-            <div className="flex flex-col gap-4 w-full">
+            <div className="flex flex-col gap-4 w-full ">
               <Link
                 href="/login"
-                className="text-gray-800 font-semibold hover:text-[hsl(356,100%,66%)] transition duration-300 block"
+                className="text-white font-semibold hover:text-[hsl(356,100%,66%)] dark:text-gray-300 dark:hover:text-white transition duration-300 block"
                 onClick={toggleMenu}
               >
                 Login
               </Link>
               <Link
                 href="/signup"
-                className="bg-[hsl(356,100%,66%)] text-white text-center py-2 rounded-3xl font-semibold hover:bg-black transition duration-300 block"
+                className="bg-[hsl(356,100%,66%)] text-white text-center py-2 rounded-3xl font-semibold hover:bg-black dark:bg-gray-700 dark:hover:bg-gray-500 transition duration-300 block"
                 onClick={toggleMenu}
               >
                 Sign up
